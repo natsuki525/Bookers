596bddaeeef10d96f7ab71f4293dde304389857e
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-07 20:08:20 +0900

    [Add] 投稿機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   end
 
   def index
+    @book = Book.new
   end
 
   def show
@@ -11,6 +12,18 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def edit
   end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
