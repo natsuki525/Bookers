e5085a786c1a10cf82e41380e6eadb0738996c13
常見夏希 <tsuneminatsuki@tsuneminatsukinoMacBook-Air.local>
2019-12-08 18:03:04 +0900

    [Fix] 投稿機能修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,12 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
@@ -19,6 +21,13 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
   root to: 'books#top'
+  patch 'books/:id' => 'books#update', as: 'update_todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
